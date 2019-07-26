537cfb544e4e41d8980c4364d5adfdd4877f1e4d
Aniket <aniket@ost.com>
2019-07-26 16:31:39 +0530

    event service added

--- src/services/OstWalletSdkUICallbackImplementation.js
+++ src/services/OstWalletSdkUICallbackImplementation.js
@@ -1,15 +1,5 @@
-import FormData from 'form-data';
-import { Alert } from 'react-native';
-import { Actions } from 'react-native-router-flux';
-import {OstWalletUIWorkflowCallback} from '@ostdotcom/ost-wallet-sdk-react-native';
-
-import { apiRoot } from '../helpers';
+import { OstWalletUIWorkflowCallback } from '@ostdotcom/ost-wallet-sdk-react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import { setLoading } from '../actions';
-import store from '../store';
-import OstWalletSdkCallbackImplementation from "./OstWalletSdkCallbackImplementation";
-
-const formData = new FormData();
 
 class OstWalletSdkUICallbackImplementation extends OstWalletUIWorkflowCallback {
   constructor() {
@@ -20,7 +10,7 @@ class OstWalletSdkUICallbackImplementation extends OstWalletUIWorkflowCallback {
     AsyncStorage.getItem('user', (err, user) => {
       user = JSON.parse(user);
 
-      OstPassphrasePrefixAccept.setPassphrasePrefix(user.user_pin_salt, user.user_details.user_id,  (error) => {
+      OstPassphrasePrefixAccept.setPassphrase(user.user_pin_salt, user.user_details.user_id,  (error) => {
         console.warn(error);
       });
     });
