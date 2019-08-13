9f1ff2e526a9d6fc50b758c6b90783b99b49939b
rachinkapoor <rachin@ost.com>
2019-08-14 03:01:47 +0530

    update custom theme config

--- src/components/HomePage/custom_theme_config.js
+++ src/components/HomePage/custom_theme_config.js
@@ -1,79 +1,70 @@
 export default {
-
-"nav_bar_logo_image": {
-  "asset_name": "dummy_logo"
-},
-
-"h1": {
-  "size": 20,
-  "font": "SFProDisplay",
-  "color": "#438bad",
-  "font_style": "semi_bold"
-},
-
-"h2": {
-  "size": 17,
-  "font": "SFProDisplay",
-  "color": "#666666",
-  "font_style": "medium"
-},
-
-"h3": {"size": 15,
-  "font": "SFProDisplay",
-  "color": "#888888",
-  "font_style": "regular"
-},
-
-"h4": {"size": 12,
-  "font": "SFProDisplay",
-  "color": "#888888",
-  "font_style": "regular"
-},
-
-"c1": {"size": 14,
-  "font": "SFProDisplay",
-  "color": "#484848",
-  "font_style": "bold"
-},
-
-"c2": {"size": 12,
-  "font": "SFProDisplay",
-  "color": "#6F6F6F",
-  "font_style": "regular"
-},
-
-"b1": {
-  "size": 17,
-  "color": "#ffffff",
-  "background_color": "#438bad",
-  "font_style": "medium"
-},
-
-"b2": {
-  "size": 17,
-  "color": "#438bad",
-  "background_color": "#ffffff",
-  "font_style": "semi_bold"
-},
-
-"b3": {
-  "size": 12,
-  "color": "#ffffff",
-  "background_color": "#438bad",
-  "font_style": "medium"
-},
-
-"b4": {
-  "size": 12,
-  "color": "#438bad",
-  "background_color": "#ffffff",
-  "font_style": "medium"
-},
-
+  "nav_bar_logo_image": {
+    "asset_name": "dummy_logo"
+  },
+  "h1": {
+    "size": 20,
+    "font": "SFProDisplay",
+    "color": "#438bad",
+    "system_font_weight": "semi_bold"
+  },
+  "h2": {
+    "size": 17,
+    "font": "SFProDisplay",
+    "color": "#666666",
+    "system_font_weight": "medium"
+  },
+  "h3": {
+    "size": 15,
+    "font": "SFProDisplay",
+    "color": "#888888",
+    "system_font_weight": "regular"
+  },
+  "h4": {
+    "size": 12,
+    "font": "SFProDisplay",
+    "color": "#888888",
+    "system_font_weight": "regular"
+  },
+  "c1": {
+    "size": 14,
+    "font": "SFProDisplay",
+    "color": "#484848",
+    "system_font_weight": "bold"
+  },
+  "c2": {
+    "size": 12,
+    "font": "SFProDisplay",
+    "color": "#6F6F6F",
+    "system_font_weight": "regular"
+  },
+  "b1": {
+    "size": 17,
+    "color": "#ffffff",
+    "background_color": "#438bad",
+    "system_font_weight": "medium"
+  },
+  "b2": {
+    "size": 17,
+    "color": "#438bad",
+    "background_color": "#ffffff",
+    "system_font_weight": "semi_bold"
+  },
+  "b3": {
+    "size": 12,
+    "color": "#ffffff",
+    "background_color": "#438bad",
+    "system_font_weight": "medium"
+  },
+  "b4": {
+    "size": 12,
+    "color": "#438bad",
+    "background_color": "#ffffff",
+    "system_font_weight": "medium"
+  },
   "navigation_bar": {
     "tint_color": "#ffffff"
   },
-
   "icons": {
     "close": {
       "tint_color": "#438bad"
@@ -82,7 +73,6 @@ export default {
       "tint_color": "#438bad"
     }
   },
-
   "pin_input": {
     "empty_color": "#c7c7cc",
     "filled_color": "#438bad"
